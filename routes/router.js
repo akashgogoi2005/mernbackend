73672edbb7ed24aa5cfbdae162d5abfe13ccd8c6
Akash Gogoi <jyotighosh188@gmail.com>
2023-03-19 12:29:28 +0530

    Update Router.js

--- routes/router.js
+++ routes/router.js
@@ -82,7 +82,7 @@ router.post(`/register`,async(req,res)=>{
 
 // Login user API
 
-router.post(`${BASE_URL}/login`, async(req,res)=>{
+router.post(`/login`, async(req,res)=>{
     const {email,password} = req.body;
 
     if(!email || !password){
